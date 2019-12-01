28cd11c10bbccb228e8464efc9ffe25204ff0431
José Sánchez <jsanxez7@gmail.com>
2019-12-01 01:08:52 -0500

    Controlador de personaje basico

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,14 @@ public class PlayerController : MonoBehaviour
 
     private float horizontalMovement;
     private float verticalMovement;
-    public float speed = 20.0f;
-    public float turnSpeed = 100.0f;
+    public float speed = 1000.0f;
+    private bool canJump;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -22,8 +23,23 @@ public class PlayerController : MonoBehaviour
         horizontalMovement = Input.GetAxis("Horizontal");
         verticalMovement = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalMovement);
-        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalMovement);
+        Vector3 movement = new Vector3(horizontalMovement, 0.0f, verticalMovement);
+        rb.AddForce(movement * speed);
+
+        if(Input.GetKeyDown(KeyCode.Space) && canJump == true)
+        {
+            rb.AddForce(Vector3.up * 350, ForceMode.Impulse);
+            canJump = false;
+        }
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("ground"))
+        {
+            canJump = true;
+            Debug.Log("Puedo saltar?: " + canJump);
+        }
     }
 }
